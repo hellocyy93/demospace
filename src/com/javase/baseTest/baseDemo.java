526eedf1450fda48d059d9d55167c37feeebc105
chenyy <chenyy@lihfinance.com>
2019-05-16 09:42:38 +0800

    新增++测试

---
+++ src/com/javase/baseTest/baseDemo.java
@@ -0,0 +1,10 @@
+package com.javase.baseTest;
+
+public class baseDemo {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = ++i;
+        System.out.println(j);
+        System.out.println(i);
+    }
+}
