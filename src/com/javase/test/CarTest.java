f665a1402129defbbd263ec5bc84bb6d4359ba0c
chenyy <chenyy@lihfinance.com>
2019-06-22 15:02:58 +0800

    update

--- src/com/javase/test/CarTest.java
+++ src/com/javase/test/CarTest.java
@@ -1,13 +1,10 @@
 package com.javase.test;
 
-import com.java.beans.Bens;
 import com.java.beans.Car;
 
 public class CarTest {
     public static void main(String[] args) {
-        Bens bens = new Bens();
-        Car car = new Car();
-        car.setBens(bens);
-        System.out.println(car);
+        Car car = null;
+        System.out.println(car.getBens());
     }
 }
