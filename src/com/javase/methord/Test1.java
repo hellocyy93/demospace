a21611fa4be8ae6f5ed2be6f5f83e051b07fdd0e
chenyy <chenyy@lihfinance.com>
2019-12-11 19:29:03 +0800

    update

---
+++ src/com/javase/methord/Test1.java
@@ -0,0 +1,31 @@
+package com.javase.methord;
+
+import com.java.beans.City;
+import org.junit.Test;
+
+public class Test1 {
+    int i = 1;
+    String str = "nihao";
+    @Test
+    public void demo() {
+       /* int y = doSum(i,str);
+        System.out.println(y);
+        System.out.println(str);*/
+        City city = new City();
+        city.setCode("001");
+        city.setName("北京");
+        getCity(city);
+        System.out.println(city);
+    }
+
+    private void getCity(City city) {
+        city.setCode("002");
+        city.setName("上海");
+       // return city;
+    }
+
+    private int doSum(int i, String str) {
+        str = "hello";
+        return i+1;
+    }
+}
